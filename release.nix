d3ce121656427c712c553b6e9a9e985378f7c2f5
Ali Abrar <ali.abrar@obsidian.systems>
2022-12-09 20:42:16 -0400

    ci: update release.nix

--- release.nix
+++ release.nix
@@ -1,8 +1,27 @@
 let
   haskellOverlaysPost = [
     (self: super: {
-      base-orphans = self.callHackage "base-orphans" "0.8.5" {};
-      dependent-sum-aeson-orphans = self.callHackage "dependent-sum-aeson-orphans" "0.3.1.0" {};
+      base-orphans = self.callHackage "base-orphans" "0.8.6" {};
+      dependent-sum-aeson-orphans = self.callHackageDirect {
+        pkg = "dependent-sum-aeson-orphans";
+        ver = "0.3.1.1";
+        sha256 = "14i74c268xpmz03nqa0ll3inf0lncn6hzhil1fiqwrqk25yfah2m";
+      } {};
+      aeson-gadt-th = self.callHackageDirect {
+        pkg = "aeson-gadt-th";
+        ver = "0.2.5.1";
+        sha256 = "1r3gx226jqs7l5jp8gmgaa2p49lnsnlzdhsxj6h47m0rnfc36qm5";
+      } {};
+      dependent-monoidal-map = self.callHackageDirect {
+        pkg = "dependent-monoidal-map";
+        ver = "0.1.1.3";
+        sha256 = "13vglclw93vgrb52gwxklzh6lww0pn1i0b5nk6kw0vg1jdzjxvwj";
+      } {};
+      reflex = self.callHackageDirect {
+        pkg = "reflex";
+        ver = "0.8.2.2";
+        sha256 = "04z7cjg0qfzpq4fhvsq0v04psny6pg8j4nns56v3hg06jf8akw6q";
+      } {};
     })
   ];
   rp = import ./reflex-platform { inherit haskellOverlaysPost; };
